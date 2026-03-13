756239c1c6f2ab1167b274316636f10ccbd07474
RICH-P1 <patrickmadu54@gmail.com>
2026-03-13 09:43:54 +0100

    add numbered questions 1-15

--- app.py
+++ app.py
@@ -4,77 +4,77 @@ app = Flask(__name__)
 
 questions = [
     {
-        "question": "What is 2 + 2?",
+        "question": "1.What is 2 + 2?",
         "options": ["1","2","4","5"],
         "answer": "4"
     },
     {
-        "question": "Capital of France?",
+        "question": "2.Capital of France?",
         "options": ["London","Paris","Rome","Madrid"],
         "answer": "Paris"
     },
     {
-        "question": "HTML stands for?",
+        "question": "3.HTML stands for?",
         "options": ["Hyper Text Markup Language","Home Tool Markup Language","Hyperlinks Text","HighText Machine"],
         "answer": "Hyper Text Markup Language"
     },
     {
-        "question": "Which language is used with Flask?",
+        "question": "4.Which language is used with Flask?",
         "options": ["Java","Python","C++","PHP"],
         "answer": "Python"
     },
     {
-        "question": "Which symbol starts a comment in Python?",
+        "question": "5.Which symbol starts a comment in Python?",
         "options": ["//","#","<!--","**"],
         "answer": "#"
     },
     {
-        "question": "Which company created Python?",
+        "question": "6.Which company created Python?",
         "options": ["Microsoft","Google","Python Software Foundation","Apple"],
         "answer": "Python Software Foundation"
     },
     {
-        "question": "Which HTML tag is used for a paragraph?",
+        "question": "7.Which HTML tag is used for a paragraph?",
         "options": ["<p>","<h1>","<div>","<span>"],
         "answer": "<p>"
     },
     {
-        "question": "Which method sends form data in HTML?",
+        "question": "8.Which method sends form data in HTML?",
         "options": ["POST","GET","SEND","PUSH"],
         "answer": "POST"
     },
     {
-        "question": "Which file runs a Flask application?",
+        "question": "9.Which file runs a Flask application?",
         "options": ["main.py","run.py","app.py","server.py"],
         "answer": "app.py"
     },
     {
-        "question": "What does CPU stand for?",
+        "question": "10.What does CPU stand for?",
         "options": ["Central Processing Unit","Computer Personal Unit","Central Power Unit","Control Processing Unit"],
         "answer": "Central Processing Unit"
     },
     {
-    "question": "Which protocol is used to load web pages?",
+    "question": "11.Which protocol is used to load web pages?",
     "options": ["FTP","HTTP","SMTP","SSH"],
     "answer": "HTTP"
     },
     {
-    "question": "What does IP stand for in internet terminology?",
+    "question": "12.What does IP stand for in internet terminology?",
     "options": ["internet patg","internet provider","internet protocol","internet process"],
     "answer": "internet protocol"
     },
     {
-    "question": "Which of these is the correct file extension for a Python file?",
+    "question": "13.Which of these is the correct file extension for a Python file?",
     "options": [".java", ".py", ".html", ".txt"],
     "answer": ".py"
     },
     {
-    "question": "What does the '=' operator do in most programming languages?",
+    "question": "14.What does the '=' operator do in most programming languages?",
     "options": ["Checks if values are equal", "Assigns a value to a variable", "Adds two numbers", "Deletes a variable"],
     "answer": "Assigns a value to a variable"
     },
     {
-    "question": "Which symbol is used to start a comment in Python?",
+    "question": "15.Which symbol is used to start a comment in Python?",
     "options": ["//", "/*", "#", "<!--"],
     "answer": "#"
     }
